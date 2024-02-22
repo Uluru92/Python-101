de02218224a5dba47a5c601a84c40ba2527c4cfa
Uluru <jorddy.castro@gmail.com>
2024-02-22 10:34:55 -0600

    commit files 6

---
+++ python-101-main/06_intro-to-programming/06_01_pseudocode_DONE.py
@@ -0,0 +1,12 @@
+# Your cat just had kittens! Now you want to put up an ad to give them
+# to your friends. You'll need to save all names of the kittens,
+# confirm that they each of them is cute, and show a message that
+# that kitten is ready for adoption.
+# Break this task up into a couple of steps of pseudocode
+# and write the pseudocode below in code comments.
+# You don't need to write any functional code, just map out the steps.
+
+#1. SET list_kittens = {"name1", "name2", "name3", "nameN"}
+#2. FOT each kitten IN the list_kittens
+    #3. IF kitten = cute:
+        #4. print("kitten is ready for adoption!!")
---
+++ python-101-main/06_intro-to-programming/06_05_program_pseudocode_DONE.py
@@ -0,0 +1,14 @@
+# Think about a task that you want to accomplish using programming
+# Maybe you want to automate something you do every day on your computer?
+# Break down some of the steps you can think of and write them in
+# this file as pseudocode.
+# You can come back and update this file when you've learned more.
+# You can also add real Python code after learning the necessary concepts.
+# For now, just practice breaking larger tasks into smaller steps
+# and writing out your thoughts in pseudocode.
+
+    # APP: Bartender Book
+    #1. Input = ingredients and licours you have
+    #2. Set a list: {"Whiskey", "Tequila", "Ron", "Vodka", "Salt", "lemon"}
+    #3. Print ("With the ingredients available, you can do the next recipes: ")
+
