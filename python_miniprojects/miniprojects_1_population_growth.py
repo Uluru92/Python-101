e05b900d48250ff343cbccd21cf8ec28a6763fa9
Uluru <jorddy.castro@gmail.com>
2024-02-26 16:55:06 -0600

    DONE

--- python_miniprojects/miniprojects_1_population_growth.py
+++ python_miniprojects/miniprojects_1_population_growth.py
@@ -12,3 +12,20 @@ One person dies every 12 seconds
 One person immigrates every 40 seconds
 """
 
+current_population = 380123456
+born_period = 1/6 #person/seconds
+death_period = 1/12 #person/seconds
+imigrate_period = 1/40 #person/seconds
+grow_rate = born_period-death_period+imigrate_period
+
+growth_period = 3 #tiempo de analisis
+print(f"Growth Period:{growth_period} years")
+
+growth_period = 3*365.25*24*60*60 #seconds
+print(f"Growth Period:{growth_period} seconds")
+
+growth_population = growth_period*grow_rate
+three_years_population = current_population + growth_population
+print("Current population:",current_population)
+print("Total growth population",growth_population)
+print("Population counter for the next 3 years:",three_years_population)
