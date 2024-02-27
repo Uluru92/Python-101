8d62f89d274e4dc3943b09836376fe0ad49e2c50
Uluru <jorddy.castro@gmail.com>
2024-02-27 12:07:34 -0600

    DONE

--- python_miniprojects/miniprojects_5_sentence_analisys_tool.py
+++ python_miniprojects/miniprojects_5_sentence_analisys_tool.py
@@ -19,3 +19,28 @@ Lower case: 26
 Punctuation: 1
 Total chars: 28
 """
+import pathlib
+
+sentence = input("Please enter a sentence: ")
+
+upper_counter = 0
+lower_counter = 0
+puntuation_counter = 0
+digit_counter = 0
+total_chars_counter = 0
+
+for letter in sentence:
+    if letter.isupper():
+      upper_counter += 1
+    elif letter.islower():
+      lower_counter += 1
+    elif letter.isdigit():
+      digit_counter += 1
+    elif letter.isspace():
+      continue
+    else:
+      puntuation_counter += 1
+
+total_chars_counter = upper_counter+lower_counter+digit_counter+puntuation_counter
+print(f"Upper case: {upper_counter}\nLower case: {lower_counter}\nPunctuation: {puntuation_counter}\nTotal chars: {total_chars_counter}")
+
