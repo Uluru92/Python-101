281f565eb1d9781f3ca98562d3994637f6aab321
Urulu92 <jorddy.castr@gmail.com>
2024-04-14 23:35:15 -0600

    ok.... explorando

--- python_miniprojects/miniprojects_5_sentence_analisys_tool_DONE.py
+++ python_miniprojects/miniprojects_5_sentence_analisys_tool_DONE.py
@@ -44,4 +44,4 @@ for letter in sentence:
 total_chars_counter = upper_counter+lower_counter+digit_counter+puntuation_counter
 print(f"Upper case: {upper_counter}\nLower case: {lower_counter}\nPunctuation: {puntuation_counter}\nTotal chars: {total_chars_counter}")
 
-#just to check something!
+#just to check something... que hago perdi estos files de agluna manera!??!
